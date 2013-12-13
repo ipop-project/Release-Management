3f4039337c43d169e31f6883999612e02ab5d0b8
Pierre St Juste <ptony82@gmail.com>
2013-12-12 23:23:41 -0500

    Update to start_controller.sh

--- start_controller.sh
+++ start_controller.sh
@@ -1,5 +1,5 @@
-export PYTHONHOME=/data/svpn/python27/files/python
-export PYTHONPATH=/data/svpn/python27/extras/python:/data/svpn/python27/files/python/lib/python2.7/lib-dynload:/data/svpn/python27/files/python/lib/python2.7
+export PYTHONHOME=/data/ipop/python27/files/python
+export PYTHONPATH=/data/ipop/python27/extras/python:/data/ipop/python27/files/python/lib/python2.7/lib-dynload:/data/ipop/python27/files/python/lib/python2.7
 export PATH=$PYTHONHOME/bin:$PATH
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/data/svpn/python27/files/python/lib:/data/svpn/python27/files/python/lib/python2.7/lib-dynload
-python vpn_controller.py $@
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/data/ipop/python27/files/python/lib:/data/ipop/python27/files/python/lib/python2.7/lib-dynload
+python $@
