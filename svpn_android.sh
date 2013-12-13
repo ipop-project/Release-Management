a6a33c3874c55aa88a7b461b02d0ec9beec5e1b1
Pierre St Juste <ptony82@gmail.com>
2013-12-13 17:49:55 -0500

    Update svpn_android.sh

--- svpn_android.sh
+++ svpn_android.sh
@@ -2,10 +2,6 @@
 # this script runs SocialVPN inside Android 4.1 emulator
 # this script is designed for Ubuntu 12.04 (64-bit)
 
-USERNAME=$1
-PASSWORD=$2
-XMPP_HOST=$3
-NO_CONTAINERS=$4
 HOST=$(hostname)
 
 sudo aptitude update
@@ -17,21 +13,23 @@ tar xzf android.tgz; tar xzf android-sdk.tgz
 
 cd android-sdk
 
-wget -O svpn-arm.tgz http://goo.gl/eBrvy1
-wget -O python27.tgz http://goo.gl/jjJxyd
-tar xzf python27.tgz; tar xzf svpn-arm.tgz
+wget http://www.acis.ufl.edu/~ptony82/ipop/ipop-android_14.01.pre2.tgz
+wget http://www.acis.ufl.edu/~ptony82/ipop/python27.tgz
+wget http://github.com/ipop-project/ipop-scripts/raw/master/start_controller.sh
+tar xzvf python27.tgz; tar xzvf ipop-android_14.01.pre2.tgz
+mv start_controller.sh ipop-android_14.01.pre2
 
 tools/emulator64-arm -avd svpn-android-4.1 -no-window -no-audio -no-skin &> log.txt &
 sleep 60
 
-platform-tools/adb shell rm -r data/svpn
-platform-tools/adb shell mkdir data/svpn
-platform-tools/adb shell mkdir data/svpn/python27
+platform-tools/adb shell rm -r data/ipop
+platform-tools/adb shell mkdir data/ipop
+platform-tools/adb shell mkdir data/ipop/python27
 
-platform-tools/adb push svpn-arm /data/svpn
-platform-tools/adb push python27 /data/svpn/python27
-platform-tools/adb shell chmod 755 /data/svpn/svpn-jingle-android
+platform-tools/adb push ipop-android_14.01.pre2 /data/ipop
+platform-tools/adb push python27 /data/ipop/python27
+platform-tools/adb shell chmod 755 /data/ipop/ipop-tincan
 
 sudo tcpdump -i eth0 -w svpn_$HOST.cap &> /dev/null &
-platform-tools/adb shell "cd /data/svpn; ./svpn-jingle-android & sh start_controller.sh $USERNAME $PASSWORD $XMPP_HOST &> log.txt &" &> log_$HOST.txt &
+platform-tools/adb shell "cd /data/ipop; ./ipop-tincan & sh start_controller.sh svpn_controller.py -c config.json &> log.txt &" &> log_$HOST.txt &
 
