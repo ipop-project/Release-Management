ceafcc54b3e54c63836d2113839aa03e62b4270a
Kyuho Jeong <kyuho.jeong@gmail.com>
2013-12-30 22:14:37 -0500

    GroupVPN LXC batch script
    Currently, this script provides two mode of operation 1 is for creating lxc instances
    2 is for copying ipop binary and controllers to
    each instance
      and run ipop.
    
    FOr the future, simple ping test and further complex test pattern
    will be included for each of repeatitive test.

---
+++ gvpn_lxc.sh
@@ -0,0 +1,136 @@
+#!/usr/bin/env bash
+
+function Usage() {
+cat <<-ENDOFMESSAGE
+options:
+  -m Operation mode 
+    1 -- Creating instances
+    2 -- Copy binaries to instances and run IPOP
+    3 -- Run Simple Test
+  -p Prefix | Specify lxc instance name prefix
+  -i Specify instance count
+
+Examples:
+  Creating Instances with prefix
+    ./gvpn_lxc -m 1 -p prefix -i 5
+    Creating lxc instances with names "prefix0" "refix1" ... "prefix4"
+  Copy ipop-tincan binary and controllers and configure IP for node
+    ./gvpn_lxc -m 2 -p prefix -i 5 -a 172.16.1.1
+    LXC instance will have ipop node with IP address 172.16.1.1, 172.16.1.2, ...
+  
+ENDOFMESSAGE
+exit 1
+}
+
+function create_instances {
+PREFIX=$1
+COUNT=$2
+lxc_path=/var/lib/lxc
+ 
+CONTROLLER=gvpn_controller.py
+
+#Check whether lxc is installed
+#if not, install it
+if ! dpkg -l lxc >> /dev/null 2>&1 
+then 
+  echo "LXC Package is not installed"
+  sudo apt-get update
+  sudo apt-get install -y lxc 
+fi
+echo "LXC is installed"
+
+
+#Check whether LXC exists with give prefix
+if sudo test ! -e /var/lib/lxc/${PREFIX}0
+then 
+  echo "Ubuntu LXC not exists. creating one"
+  sudo lxc-create -t ubuntu -n ${PREFIX}0
+else
+  echo "Ubuntu LXC exists with given prefix $PREFIX"
+  exit 1;
+fi
+
+#Install python and tap device in instance
+sudo chroot /var/lib/lxc/${PREFIX}0/rootfs apt-get update
+sudo chroot /var/lib/lxc/${PREFIX}0/rootfs apt-get install -y python
+sudo chroot /var/lib/lxc/${PREFIX}0/rootfs mkdir /dev/net
+sudo chroot /var/lib/lxc/${PREFIX}0/rootfs mknod /dev/net/tun c 10 200
+sudo chroot /var/lib/lxc/${PREFIX}0/rootfs chmod 666 /dev/net/tun
+
+for ((i=1; i<$COUNT; i++))
+do
+  container_path=$lxc_path/$PREFIX$i
+
+  if sudo test -e /var/lib/lxc/$PREFIX$i
+  then 
+    echo "LXC instance name $PREFIX$i exists."
+    exit 1;
+  fi
+
+  sudo lxc-clone -o ${PREFIX}0 -n $PREFIX$i
+done
+}
+
+function copy {
+PREFIX=$1
+COUNT=$2
+ADDR=$3
+lxc_path=/var/lib/lxc
+IP=(${ADDR//./ })
+
+cat > run.sh << EOF
+#!/usr/bin/env bash
+cd /home/ubuntu
+sudo ./ipop-tincan-x86_64 &> tincan.log &
+./gvpn_controller.py -c config.json &> controller.log &
+EOF
+
+sudo chmod +x run.sh
+
+for ((i=0; i<$COUNT; i++))
+do
+  container_path=$lxc_path/$PREFIX$i
+
+  if sudo test ! -e /var/lib/lxc/$PREFIX$i
+  then 
+    echo "Target does not exist ($PREFIX$i)."
+    exit 0;
+  fi
+
+  sudo cp ipop-tincan-x86_64 $container_path/rootfs/home/ubuntu/
+  sudo cp config.json $container_path/rootfs/home/ubuntu/
+  sudo cp gvpn_controller.py $container_path/rootfs/home/ubuntu/
+  sudo cp svpn_controller.py $container_path/rootfs/home/ubuntu/
+  sudo cp run.sh $container_path/rootfs/home/ubuntu/
+
+  sudo sed -i "s/\"ip4\":.*/\"ip4\": \"${IP[0]}.${IP[1]}.${IP[2]}.$((${IP[3]}+$i))\",/g" $container_path/rootfs/home/ubuntu/config.json
+ 
+  sudo lxc-start -d -n $PREFIX$i 
+  sleep 5
+
+  #lxc-attach is not supported for all linux packages
+  #If below is not supproted, I recommend to use ssh
+  sudo lxc-attach -n $PREFIX$i /home/ubuntu/run.sh 
+done
+
+}
+
+TEMP=`getopt -o m:p:i:a: -- "$@"`
+eval set -- "$TEMP"
+while true 
+do
+  case "$1" in 
+    -m) MODE=$2; shift 2;;
+    -p) PREFIX=$2; shift 2;;
+    -i) COUNT=$2; shift 2;;
+    -a) ADDR=$2; shift 2;;
+    --) shift; break ;;
+    *) Usage; exit 1;;
+  esac
+done
+
+case $MODE in 
+  1) create_instances $PREFIX $COUNT; exit 1;;
+  2) copy $PREFIX $COUNT $ADDR; exit 1;;
+  *) echo "Unknown operation mode"; Usage; exit 1;;
+esac
