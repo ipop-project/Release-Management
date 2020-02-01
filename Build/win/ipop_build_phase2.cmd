92cee1289a64e314240841f6b24406af71ac76dc
ken <kcratie@users.noreply.github.com>
2020-01-31 21:24:55 -0500

    Remove deprecated files

--- Build/win/ipop_build_phase2.cmd
+++
@@ -1,4 +0,0 @@
-gclient runhooks --force
-
-cd C:\Users\ipopuser\workspace\libjingle\trunk
-ninja -C out/Release ipop-tincan
--- Miscellaneous/gvpn-config-generator/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-setup(
-    name="groupvpn-config",
-    install_requires=[
-        "ipaddress ==1.0.6",
-    ],
-    scripts=["groupvpn-config"],
-)
